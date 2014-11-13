c9309503d56f6b4dbdf43ca78c9f80a94dd5cffd
mtallet <mtallet@sigma.fr>
2014-11-13 17:19:07 +0100

    Fix serialisation json + configuration logger pour du debug

--- src/main/java/com/sigma/gclc/controller/GclcController.java
+++ src/main/java/com/sigma/gclc/controller/GclcController.java
@@ -10,16 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GclcController {
 
-	static class Team {
+	public static class Team {
 		private List<String> members = new ArrayList<String>(5);
 
 		public Team addMember(String string) {
 			members.add(string);
 			return this;
 		}
+		
+		public List<String> getMembers() {
+			return members;
+		}
 	}
 
-	@RequestMapping(value = "/gclc", produces = MediaType.APPLICATION_JSON_VALUE, consumes= MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/gclc", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Team team() {
 		return new Team().addMember("myrtille").addMember("simon")
 				.addMember("steve").addMember("julien").addMember("michael");
